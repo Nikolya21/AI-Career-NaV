811492562b8b2c25a7bd2b5c9259cfa5f23ef355
Nikolya Khachatryan <164537426+Nikolya21@users.noreply.github.com>
2026-03-22 15:53:20 +0300

    Create repositories

---
+++ src/test/java/org/example/aicareernav1/AiCareerNavApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aicareernav1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCareerNavApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
