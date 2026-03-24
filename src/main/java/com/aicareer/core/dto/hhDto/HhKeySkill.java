95a72b7f3dbde19a41024911a5a04bdc72210f2a
Nikolya Khachatryan <164537426+Nikolya21@users.noreply.github.com>
2026-03-25 02:30:47 +0300

    fix bugs

--- src/main/java/com/aicareer/core/dto/hhDto/HhKeySkill.java
+++
@@ -1,8 +0,0 @@
-package com.aicareer.core.dto.hhDto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record HhKeySkill(String name) {
-
-}
--- src/main/java/com/aicareer/core/dto/hhDto/HhVacanciesResponse.java
+++
@@ -1,8 +0,0 @@
-package com.aicareer.core.dto.hhDto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.List;
-
-@JsonIgnoreProperties
-public record HhVacanciesResponse(List<HhVacancyItem> items) {}
-
